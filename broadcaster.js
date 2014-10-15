c7ca4b6a79692d4e3227d47749a2219c00d34313
Arne Boon <arneboon@gmail.com>
2014-10-15 15:47:00 +0200

    clean up

--- broadcaster.js
+++ broadcaster.js
@@ -8,15 +8,8 @@ var PORT = process.env.PORT ? process.env.PORT : 7000;
 // var socket = io.connect('http://localhost:'+PORT+'/user?type=broadcaster',{forceNew:true});
 var socket = io.connect('http://ultimaker.local:'+PORT+'/user?type=broadcaster',{forceNew:true});
 
-var currentImage = 0;
-var numImages = 2;
-
 socket.once('connect',function() {
   debug('connected');
-  // send the image
-  // broadcast();
-  //sendFile('tobroadcast.jpg');
-  //sendFile('tobroadcast2.jpg');
   sendFile('dice.stl');
 });
 
@@ -24,20 +17,20 @@ socket.on('error', function (err){
   debug('error connecting: '+err);
 });
 
-function broadcast() {
-  sendFile('tobroadcast'+currentImage+'.jpg');
-  currentImage++;
-  if(currentImage < numImages) {
-    setTimeout(broadcast,2000);
-  }
-}
-
 function sendFile(fileName) {
-  var stream = ss.createStream();
   debug('broadcasting: ',fileName);
 
+  //--stream
+  var stream = ss.createStream();
   ss(socket).emit('print', stream, {name: fileName});
   fs.createReadStream(fileName).pipe(stream);
+  stream.on('drain', function() {
+    debug('drain');
+  });
+  stream.on('end', function() {
+    debug('end');
+  })
 
-//  socket.emit('image', {name: fileName});
+  //--socket
+  // socket.emit('print', {name: fileName});
 }
